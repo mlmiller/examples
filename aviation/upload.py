b06d3af574fff60d85dc5f97e70137089fc4f8bb
Mike Miller <mike@cloudant.com>
2011-01-15 16:02:31 -0800

    Whoops, accidentally deleted the gzip handling.  Back now

--- aviation/upload.py
+++ aviation/upload.py
@@ -1,16 +1,31 @@
 from couchdbkit import Server, Database
 from couchdbkit.loaders import FileSystemDocsLoader
 from csv import DictReader
-import time, sys
+import time, sys, subprocess
 
+def gunzip(ifname):
+    cmd = 'gunzip %s' % ifname
+    print cmd
+    proc = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,)
+    stdout_value = proc.communicate()[0]
+    stdout_value = stdout_value.strip()
+    print stdout_value
+    return ifname.replace('.gz','')
+    
+def upload(db, docs):
+    print 'upload:\t%i' % n
+    db.bulk_save(docs)
+    del docs
+    return list()
+    
 if __name__=='__main__':
-    
-    
   fname = sys.argv[1]
   uri = sys.argv[2]
   dbname = sys.argv[3]
   
   print 'Upload contents of %s to %s/%s' % (fname, uri, dbname)
+  if (fname.endswith('.gz')):
+      fname = gunzip(fname)
   
   # #connect to the db
   cloudant = Server(uri)
@@ -20,11 +35,11 @@ if __name__=='__main__':
   #sync the views for prebuilt indices
   loader = FileSystemDocsLoader('_design/')
   loader.sync(db, verbose=True)
-
-
+  
   #loop on file for upload
   reader = DictReader(open(fname),delimiter='|')
   
+  #used for bulk uploading
   docs = list()
   checkpoint = 1000
   n=0
@@ -33,18 +48,13 @@ if __name__=='__main__':
   for doc in reader:
     n+=1
     docs.append(doc)
-    # print doc
     if (len(docs)%checkpoint==0):
-      print 'upload:\t%i' % n
-      db.bulk_save(docs)
-      del docs
-      docs = list()
-  
+        docs = upload(db,docs)
+        
   #don't forget the last batch        
-  db.bulk_save(docs)
+  upload(db, docs)
   
-  #print summary statistics
-  
+  #print summary statistics  
   delta = time.time() - start
   rate = float(checkpoint)/float(delta)
   ndocs = n
